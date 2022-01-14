652e13c84f61f739c3c61b1d5a1a538261ff7b61
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-14 09:43:55 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,30 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div>This is Navbar Component.</div>
+      <div>
+        <ul
+          style={{
+            display: "flex",
+            textDecoration: "none",
+            listStyleType: "none",
+            justifyContent: "space-between",
+          }}
+        >
+          <li>Musicverse</li>
+          <li>Search</li>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              width: "20%",
+            }}
+          >
+            <li>Love</li>
+            <li>Profile</li>
+            <li>thmode</li>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
