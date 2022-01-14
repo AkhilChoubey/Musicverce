04bf7d069f26f126caf6804b7e6699ccdbf6d16c
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-14 23:07:26 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+
+const themes = {
+  light: "/light.css",
+  dark: "/dark.css",
+};
 
 function App() {
   return (
