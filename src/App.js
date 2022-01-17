09f85efd937b663bbee958766ea9f00117a42b63
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-17 23:23:46 +0530

    Home Component Addition

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./Components/Navbar";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+import Home from "./Components/home/Home";
 
 const themes = {
   light: "/light.css",
@@ -13,7 +16,12 @@ function App() {
   return (
     <div className="App">
       <ThemeSwitcherProvider defaultTheme="light" themeMap={themes}>
-        <Navbar />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </Router>
       </ThemeSwitcherProvider>
     </div>
   );
---
+++ src/Components/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <h1>Feel the heart beat</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
