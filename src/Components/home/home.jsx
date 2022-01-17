2cf037bfee6b75f20cf9418e048f88e2bdff0365
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-18 00:55:20 +0530

    Home Component Addition

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
         <div>
           <span>MUSICVERSE</span>
           <h1>Feel the heart beat</h1>
-          <p>
+          {/* <p>
             Weclome to the universe of music , all the best for your musical
             journey , which is about to begin
-          </p>
+          </p> */}
         </div>
       </div>
     </>
@@ -18,3 +18,5 @@ const Home = () => {
 };
 
 export default Home;
+
+// styled image -- https://preview.colorlib.com/theme/djoz/img/about/xabout.png.pagespeed.ic.UQ_7DY9Axc.webp
