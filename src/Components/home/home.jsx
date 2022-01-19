0c294503ccda30bc61447dfed108c0107fdc0888
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-19 20:27:13 +0530

    Home Component Addition

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -7,6 +7,11 @@ const Home = () => {
         <div>
           <span>MUSICVERSE</span>
           <h1>Feel the heart beat</h1>
+          <img
+            className="home-img1"
+            src="https://preview.colorlib.com/theme/djoz/img/about/xabout.png.pagespeed.ic.UQ_7DY9Axc.webp"
+            alt="Img Not Found"
+          />
           {/* <p>
             Weclome to the universe of music , all the best for your musical
             journey , which is about to begin
