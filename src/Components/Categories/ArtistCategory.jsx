123ec26256fbdd1f4c7bce83293c5b0954a75719
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-24 10:09:40 +0530

    Home Component

--- src/Components/Categories/ArtistCategory.jsx
+++ src/Components/Categories/ArtistCategory.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import "./AtristCategory.jsx";
 const ArtistCategory = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>This is ArtistCategory Component</h1>
+      </div>
+    </>
+  );
 };
 
 export default ArtistCategory;
